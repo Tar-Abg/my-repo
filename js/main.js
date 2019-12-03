a377f09b2fb15ef587168c4f12b69d3238281b9a
Taron Abgaryan <taronabg77@gmail.com>
2019-12-04 01:00:50 +0400

    changes1

--- js/main.js
+++ js/main.js
@@ -110,12 +110,13 @@ function fetchData(current, next, pre) {
 function changePage(current, nextpage, prepage) {
     prepage = parseInt(current) - 1;
     nextpage = parseInt(current) + 1;
-
+    console.log(current + "old")
 
     var pagin = document.getElementById('pagin');
 
     let newpage;
     if (prepage <= 0) {
+        console.log(current + "new")
         newpage =
             '<li class=" disabled">' +
             '<a class="page-link" onclick="prev(' + 1 + ');" tabindex="">&laquo;</a></li>' +
